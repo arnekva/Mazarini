060f860875273da0413deab5c99abdb91a7b8779
Arne <arne@papirfly.no>
2021-06-03 17:27:45 +0200

    no longer admin only

--- index.ts
+++ index.ts
@@ -4,7 +4,7 @@ import { commands } from "./commands"
 import { Admin } from "./admin";
 import { Spinner } from "./spinner";
 import { Guild, GuildManager, GuildMember, Message, Role, TextChannel, User, Emoji } from "discord.js";
-import {doesThisMessageNeedAnEivindPride} from "./miscUtils";
+import { doesThisMessageNeedAnEivindPride } from "./miscUtils";
 const Discord = require('discord.js');
 const mazariniClient = new Discord.Client();
 const schedule = require('node-schedule');
@@ -30,18 +30,18 @@ mazariniClient.on('ready', () => {
 	//https://www.npmjs.com/package/node-schedule
 	action_log_channel = mazariniClient.channels.cache.get("810832760364859432")
 
-	const resetMygleJob = schedule.scheduleJob({ hour: 7, minute: 0 }, function() {
+	const resetMygleJob = schedule.scheduleJob({ hour: 7, minute: 0 }, function () {
 		console.log("Kjører resett av mygling kl 08:00")
 		Admin.deleteDatabaseKeysFromPrefixNotCommand("mygling")
 	});
 
-	const reminSpinJob = schedule.scheduleJob("0 20 * * 7", function() {
+	const reminSpinJob = schedule.scheduleJob("0 20 * * 7", function () {
 		console.log("Minner om reset")
 		const las_vegas = mazariniClient.channels.cache.get("808992127249678386")
 		MessageHelper.sendMessage(las_vegas, "Husk at ukens spin resetter i morgen klokken 09:00! ")
 	});
 
-	const resetSpinJob = schedule.scheduleJob("0 7 * * 1", async function() {
+	const resetSpinJob = schedule.scheduleJob("0 7 * * 1", async function () {
 		console.log("Kjører resett av spins, mandag 09:00")
 		const las_vegas = mazariniClient.channels.cache.get("808992127249678386")
 
@@ -49,8 +49,8 @@ mazariniClient.on('ready', () => {
 		const message = await las_vegas.send(spinnerMention + ", ukens spin har blitt nullstilt. Her er ukens score:\n")
 		await Spinner.listScores(message, true)
 		Admin.deleteDatabaseKeysFromPrefixNotCommand("spin")
-		
-		
+
+
 		//TODO: Finne ut hvorfor denne feilet?
 		// mazariniClient.members.forEach((member: GuildMember) => member.roles.remove(spinnerRole))
 	});
@@ -63,15 +63,15 @@ mazariniClient.on('message', async (message: Message) => {
 		return;
 
 	//UNCOMMENT denne når du jobber med viktige ting for å unngå rar oppførsel	
-	if (!(Admin.isAuthorAdmin(message.member))) {
-		if (message.content.includes("!mz"))
-			message.channel.send("Vedlikehold/utvikling på databasen pågår. Alle funksjoner er derfor midlertidig utilgjengelig. Se channelen 'bot-utvikling' for mer info")
-		return;
-	}
+	// if (!(Admin.isAuthorAdmin(message.member))) {
+	// 	if (message.content.includes("!mz"))
+	// 		message.channel.send("Vedlikehold/utvikling på databasen pågår. Alle funksjoner er derfor midlertidig utilgjengelig. Se channelen 'bot-utvikling' for mer info")
+	// 	return;
+	// }
 
 	/**  Check message for commands */
 	await checkForCommand(message);
-	
+
 
 	/*
 	Commands below this comment are not called, but run on each sent message. 
@@ -80,52 +80,52 @@ mazariniClient.on('message', async (message: Message) => {
 
 });
 
-async function checkForCommand(message: Message){
-if (message.content.toLowerCase().startsWith("!mz")) {
-	
-	let cmdFound = false;
+async function checkForCommand(message: Message) {
+	if (message.content.toLowerCase().startsWith("!mz")) {
+
+		let cmdFound = false;
 		const command = message.content.replace("!mz ", "").replace("!Mz ", "").replace("!MZ ", "").toLowerCase().split(" ")[0]
-		
+
 		commands.forEach((cmd) => {
 			if (command == cmd.commandName.toLowerCase()) {
-				
-        //Remove '!mz <command name>' from the content to avoid repeating this process in each function. 
-				const messageContent = message.content.replace("!mz " + cmd.commandName, "").replace("!Mz "+ cmd.commandName, "").replace("!MZ "+ cmd.commandName, "").trim()
+
+				//Remove '!mz <command name>' from the content to avoid repeating this process in each function. 
+				const messageContent = message.content.replace("!mz " + cmd.commandName, "").replace("!Mz " + cmd.commandName, "").replace("!MZ " + cmd.commandName, "").trim()
 				const args = !!messageContent ? messageContent.split(" ") : undefined;
 				if (cmd.isAdmin) {
 					if (Admin.isAuthorAdmin(message.member)) {
 						cmd.command(message, messageContent, args)
-						
+
 					} else {
 						MessageHelper.sendMessage(message.channel, "", true, message.author.username + " forsøkte å bruke " + cmd.commandName + " uten tilgang", "unauthorized")
 					}
 				} else {
-					try{
-						if(!!cmd.deprecated)
-						MessageHelper.sendMessage(message.channel, "*Denne funksjoner er markert som deprecated/utfaset. Bruk **" + cmd.deprecated + "*** *i stedet*")
-					cmd.command(message, messageContent, args);
-					} catch (error){
+					try {
+						if (!!cmd.deprecated)
+							MessageHelper.sendMessage(message.channel, "*Denne funksjoner er markert som deprecated/utfaset. Bruk **" + cmd.deprecated + "*** *i stedet*")
+						cmd.command(message, messageContent, args);
+					} catch (error) {
 						//!mz maggi feiler en gang i blant, så prøver å fange den og printe stacktrace i action_log.
-						MessageHelper.sendMessage(message.channel, "", true, message.author.username + " forsøkte å bruke " + cmd.commandName + " men en feil oppstod. Stacktrace: \n"+ error, "error")
+						MessageHelper.sendMessage(message.channel, "", true, message.author.username + " forsøkte å bruke " + cmd.commandName + " men en feil oppstod. Stacktrace: \n" + error, "error")
 					}
 				}
 				cmdFound = true;
 			}
 		})
 		const kekw = await message.client.emojis.cache.find(emoji => emoji.name == "kekw");
-		if(!cmdFound){
+		if (!cmdFound) {
 			const commandNames: string[] = [];
 			commands.forEach((el) => commandNames.push(el.commandName))
-			if(kekw)
-			message.react(kekw)
-		const matched = didYouMean(command, commandNames)
+			if (kekw)
+				message.react(kekw)
+			const matched = didYouMean(command, commandNames)
 
-		message.reply("lmao, commanden '"+command+"' fins ikkje <:kekw:"+ kekw + "> ." + (matched ? " Mente du **" + matched + "**?" : " Prøv !mz help"))
+			message.reply("lmao, commanden '" + command + "' fins ikkje <:kekw:" + kekw + "> ." + (matched ? " Mente du **" + matched + "**?" : " Prøv !mz help"))
 		}
 	}
 }
 /** Checks for pølse, eivindpride etc. */
-function checkMessageForJokes(message: Message){
+function checkMessageForJokes(message: Message) {
 
 	let matches;
 	let polseCounter = 0;
@@ -156,40 +156,40 @@ function checkMessageForJokes(message: Message){
 const discordToken = "ODAyOTQ1Nzk2NDU3NzU4NzYw.YA2niw.uUCQwK0J_7Vj88H_zbTyLuLMsow";
 mazariniClient.login(discordToken);
 
-mazariniClient.on("reconnecting", function() {
+mazariniClient.on("reconnecting", function () {
 	console.log(`client tries to reconnect to the WebSocket`);
 })
-mazariniClient.on("disconnect", function() {
+mazariniClient.on("disconnect", function () {
 	console.log(`Disconnecting?`);
 })
-mazariniClient.on("error", function(error: any) {
+mazariniClient.on("error", function (error: any) {
 	console.error(`client's WebSocket encountered a connection error: ${error}`);
 });
 
 
-mazariniClient.on("channelCreate", function(channel: TextChannel){
+mazariniClient.on("channelCreate", function (channel: TextChannel) {
 	MessageHelper.sendMessageToActionLog(channel, "Ny channel opprettet: " + channel.name)
 
 });
-mazariniClient.on("channelDelete", function(channel: TextChannel){
+mazariniClient.on("channelDelete", function (channel: TextChannel) {
 	MessageHelper.sendMessageToActionLog(channel, "Channel slettet: " + channel.name)
 });
 
-mazariniClient.on("guildBanAdd", function(guild: Guild, user: User){
-    MessageHelper.sendMessageToActionLog(guild.channels.cache.first() as TextChannel, "Bruker ble bannet: " + user.tag)
+mazariniClient.on("guildBanAdd", function (guild: Guild, user: User) {
+	MessageHelper.sendMessageToActionLog(guild.channels.cache.first() as TextChannel, "Bruker ble bannet: " + user.tag)
 });
 
-mazariniClient.on("guildCreate", function(guild: Guild){
-    MessageHelper.sendMessageToActionLog(guild.channels.cache.first() as TextChannel, "Ukjent: on guildCreate. Wat dis do?")
+mazariniClient.on("guildCreate", function (guild: Guild) {
+	MessageHelper.sendMessageToActionLog(guild.channels.cache.first() as TextChannel, "Ukjent: on guildCreate. Wat dis do?")
 });
 
-mazariniClient.on("guildMemberAdd", function(member: GuildMember){
+mazariniClient.on("guildMemberAdd", function (member: GuildMember) {
 	MessageHelper.sendMessage(member.guild.channels.cache.get("340626855990132747") as TextChannel, "Welcome to the Gulag, " + (member.nickname ?? member.displayName) + ". Fight for your release.")
-    MessageHelper.sendMessageToActionLog(member.guild.channels.cache.first() as TextChannel, "En bruker ble med i Mazarini: " + (member.nickname ?? member.displayName))
+	MessageHelper.sendMessageToActionLog(member.guild.channels.cache.first() as TextChannel, "En bruker ble med i Mazarini: " + (member.nickname ?? member.displayName))
 });
-mazariniClient.on("guildMemberRemove", function(member: GuildMember){
+mazariniClient.on("guildMemberRemove", function (member: GuildMember) {
 	MessageHelper.sendMessage(member.guild.channels.cache.get("340626855990132747") as TextChannel, "Farvell, " + (member.nickname ?? member.displayName))
-    MessageHelper.sendMessageToActionLog(member.guild.channels.cache.first() as TextChannel, "En bruker forlot Mazarini: " + (member.nickname ?? member.displayName))
+	MessageHelper.sendMessageToActionLog(member.guild.channels.cache.first() as TextChannel, "En bruker forlot Mazarini: " + (member.nickname ?? member.displayName))
 });
 
 /** TODO START */
@@ -199,86 +199,86 @@ mazariniClient.on("guildMemberRemove", function(member: GuildMember){
 
 
 //TODO: Sjekk cases og finn ut hva som er endret
-mazariniClient.on("userUpdate", function(oldUser: User, newUser: User){
+mazariniClient.on("userUpdate", function (oldUser: User, newUser: User) {
 	MessageHelper.sendMessageToActionLog(newUser.client.channels.cache.array()[0] as TextChannel, "Oppdatert bruker1:   " + oldUser.tag ?? oldUser.username + " -> " + (newUser.tag ?? newUser.username) + "")
 });
 
 //Emitted whenever a guild member changes - i.e. new role, removed role, nickname.
-mazariniClient.on("guildMemberUpdate", function(oldMember: GuildMember, newMember: GuildMember){
-		const diffCalc = diff.diff;
-		const differences = diff(oldMember, newMember);
-		const whatChanged = compareMember(oldMember, newMember);
-		// console.log(newMember)
-		let changesString = "";
-		differences.forEach((change: any, index: number) => {
-			// console.log(change)
-			changesString += change.path + (index == differences.length ? " " : ",")
-		})
-    MessageHelper.sendMessageToActionLog(newMember.client.channels.cache.array()[0] as TextChannel, "Oppdatert bruker " + (oldMember.nickname?? oldMember.displayName) + ": " + whatChanged + ".")
+mazariniClient.on("guildMemberUpdate", function (oldMember: GuildMember, newMember: GuildMember) {
+	const diffCalc = diff.diff;
+	const differences = diff(oldMember, newMember);
+	const whatChanged = compareMember(oldMember, newMember);
+	// console.log(newMember)
+	let changesString = "";
+	differences.forEach((change: any, index: number) => {
+		// console.log(change)
+		changesString += change.path + (index == differences.length ? " " : ",")
+	})
+	MessageHelper.sendMessageToActionLog(newMember.client.channels.cache.array()[0] as TextChannel, "Oppdatert bruker " + (oldMember.nickname ?? oldMember.displayName) + ": " + whatChanged + ".")
 });
 
 /** TODO END */
 
-mazariniClient.on("roleCreate", function(role: Role){
-    MessageHelper.sendMessageToActionLog(role.guild.channels.cache.first() as TextChannel, "En ny rolle er opprettet: " + role.name)
+mazariniClient.on("roleCreate", function (role: Role) {
+	MessageHelper.sendMessageToActionLog(role.guild.channels.cache.first() as TextChannel, "En ny rolle er opprettet: " + role.name)
 });
-mazariniClient.on("roleDelete", function(role: Role){
-    MessageHelper.sendMessageToActionLog(role.guild.channels.cache.first() as TextChannel, "En rolle er slettet: " + role.name)
-});
-
-mazariniClient.on("roleUpdate", function(oldRole: Role, newRole: Role){
-    MessageHelper.sendMessageToActionLog(newRole.guild.channels.cache.first() as TextChannel, "Rollen " + newRole.name + " ble oppdatert.")
+mazariniClient.on("roleDelete", function (role: Role) {
+	MessageHelper.sendMessageToActionLog(role.guild.channels.cache.first() as TextChannel, "En rolle er slettet: " + role.name)
 });
 
-mazariniClient.on("messageDelete", function(message: Message){
-     //Ikke i bruk
+mazariniClient.on("roleUpdate", function (oldRole: Role, newRole: Role) {
+	MessageHelper.sendMessageToActionLog(newRole.guild.channels.cache.first() as TextChannel, "Rollen " + newRole.name + " ble oppdatert.")
 });
-mazariniClient.on("messageUpdate", function(oldMessage: Message, newMessage: Message){
-    checkForCommand(newMessage);
-		checkMessageForJokes(newMessage);
+
+mazariniClient.on("messageDelete", function (message: Message) {
+	//Ikke i bruk
+});
+mazariniClient.on("messageUpdate", function (oldMessage: Message, newMessage: Message) {
+	checkForCommand(newMessage);
+	checkMessageForJokes(newMessage);
 });
 
 
-mazariniClient.on("warn", function(info: string){
-    MessageHelper.sendMessageToActionLog(mazariniClient.channels.cache.get("810832760364859432") as TextChannel, "En advarsel ble fanget opp. Info: \n " + info)
+mazariniClient.on("warn", function (info: string) {
+	MessageHelper.sendMessageToActionLog(mazariniClient.channels.cache.get("810832760364859432") as TextChannel, "En advarsel ble fanget opp. Info: \n " + info)
 });
-mazariniClient.on("error", function(error: Error){
-    MessageHelper.sendMessageToActionLog(mazariniClient.channels.cache.get("810832760364859432") as TextChannel, "En feilmelding ble fanget opp. Error: \n " + error)
+mazariniClient.on("error", function (error: Error) {
+	MessageHelper.sendMessageToActionLog(mazariniClient.channels.cache.get("810832760364859432") as TextChannel, "En feilmelding ble fanget opp. Error: \n " + error)
 });
 
-function findRoleDifference(){
+function findRoleDifference() {
 	//TODO: Returner 
 }
 
-function compareMember(oldMember: GuildMember, newMember: GuildMember){
+function compareMember(oldMember: GuildMember, newMember: GuildMember) {
 	const roles = oldMember.roles.cache;
 	const role = roleArraysEqual(oldMember.roles.cache.array(), newMember.roles.cache.array())
-	if(role){
+	if (role) {
 		return "role: " + role.name;
 	}
-	if(oldMember.nickname !== newMember.nickname)
-	return "nickname: " + (oldMember.nickname ?? oldMember.displayName) + " endret til " + (newMember.nickname ?? newMember.displayName)
-	if(oldMember.user.username !== newMember.user.username)
-	return "username"
+	if (oldMember.nickname !== newMember.nickname)
+		return "nickname: " + (oldMember.nickname ?? oldMember.displayName) + " endret til " + (newMember.nickname ?? newMember.displayName)
+	if (oldMember.user.username !== newMember.user.username)
+		return "username"
 
 	//TODO: Sjekk etter andre ting?
-	if(oldMember.nickname !== newMember.nickname)
-	return "nickname"
-	if(oldMember.nickname !== newMember.nickname)
-	return "nickname"
+	if (oldMember.nickname !== newMember.nickname)
+		return "nickname"
+	if (oldMember.nickname !== newMember.nickname)
+		return "nickname"
 }
 
 function roleArraysEqual(a: any[], b: any[]) {
-  if (a === b) return undefined;
-  if (a == null || b == null) return undefined;
+	if (a === b) return undefined;
+	if (a == null || b == null) return undefined;
 
-  for (var i = 0; i < a.length; ++i) {
-    if (a[i] !== b[i]) {
-			if(a.length > b.length)
+	for (var i = 0; i < a.length; ++i) {
+		if (a[i] !== b[i]) {
+			if (a.length > b.length)
 				return a[i] as Role;
-			 else
+			else
 				return b[i] as Role;
 		}
-  }
-  return undefined;
+	}
+	return undefined;
 }
