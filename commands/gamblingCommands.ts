6ba1c6cf678f243d33af3eb9980dcec74d2ae1f2
Arne Kvaleberg <arne@papirfly.no>
2021-06-20 19:40:47 +0200

    Adding new gambling commands

--- commands/gamblingCommands.ts
+++ commands/gamblingCommands.ts
@@ -1,5 +1,5 @@
 
-import { Message } from "discord.js";
+import { Message, MessageEmbed } from "discord.js";
 import { Channel, Client, DMChannel, NewsChannel, TextChannel } from "discord.js";
 import { betObject, betObjectReturned, DatabaseHelper, dbPrefix } from "../helpers/databaseHelper";
 import { MessageHelper } from "../helpers/messageHelper";
@@ -106,8 +106,6 @@ export class GamblingCommands {
                     })
                     if (positiveCounter > 2) {
                         const isPositive = args[0].toLocaleLowerCase() === "ja";
-
-
                         MessageHelper.sendMessage(message, `Veddemålsresultatet er godkjent. Beløpene blir nå lagt til på kontoene. `)
                         const value = activeBet.value;
                         GamblingCommands.dealCoins(message, activeBet.value, isPositive ? activeBet.positivePeople : activeBet.negativePeople)
@@ -121,10 +119,93 @@ export class GamblingCommands {
             }
 
         } else {
-            message.reply("object is not instance of betObject")
+            message.reply("object is not instance of betObject. why tho")
         }
+    }
 
+    static diceGamble(message: Message, content: string, args: string[]) {
+        const userMoney = DatabaseHelper.getValue("dogeCoin", message.author.username, message);
+        const val = args[0];
+        if (!val) {
+            message.reply("Du må si hvor mye du vil gamble")
+            return;
+        }
+        if (userMoney) {
 
+            if (parseInt(val) > parseInt(userMoney)) {
+                message.reply("Du har ikke nok penger til å gamble så mye")
+                return;
+            }
+        }
+        if (val && Number(val)) {
+            const roll = Math.floor(Math.random() * 101);
+            let newMoneyValue = 0;
+            if (roll >= 50)
+                newMoneyValue = Number(userMoney) + Number(val);
+            else
+                newMoneyValue = Number(userMoney) - (Number(val));
+
+            DatabaseHelper.setValue("dogeCoin", message.author.username, newMoneyValue.toFixed(2))
+
+            const gambling = new MessageEmbed()
+                .setTitle("Gambling 🎲")
+                .setDescription(`${message.author.username} gamblet ${val} av ${userMoney} coins.\nTerningen trillet: ${roll}/100. Du ${roll >= 50 ? "vant! 💰💰" : "tapte 💸"}\nDu har nå ${newMoneyValue.toFixed(2)} coins.`)
+
+            MessageHelper.sendFormattedMessage(message, gambling);
+        }
+    }
+
+    static takeUpLoan(message: Message, content: string, args: string[]) {
+        let amountToLoan = 500;
+        if (args[0]) {
+            const argAsNum = Number(args[0])
+
+            if (isNaN(argAsNum)) {
+                message.reply("du har oppgitt et ugyldig tall")
+                return;
+            } else if (argAsNum > 1000) {
+                message.reply("du kan låne maks 1000 coins")
+                return;
+            } else
+                amountToLoan = argAsNum;
+        }
+        const username = message.author.username;
+        const totalLoans = DatabaseHelper.getValue("loanCounter", username, message)
+        const totalDebt = DatabaseHelper.getValue("debt", username, message)
+        const userMoney = DatabaseHelper.getValue("dogeCoin", message.author.username, message);
+
+        const newTotalLoans = Number(totalLoans) + 1;
+        const newDebt = Number(totalDebt) + amountToLoan;
+
+        DatabaseHelper.setValue("loanCounter", username, newTotalLoans.toString())
+        DatabaseHelper.setValue("debt", username, newDebt.toString())
+        const newCoinsVal = Number(userMoney) + amountToLoan;
+        DatabaseHelper.setValue("dogeCoin", username, newCoinsVal.toString())
+
+        MessageHelper.sendMessage(message, `${username}, du har nå lånt ${amountToLoan} coins. Spend them well. Din totale gjeld er nå: ${newDebt} (${newTotalLoans} lån gjort)`)
+    }
+
+    static payDownDebt(message: Message, content: string, args: string[]) {
+        const username = message.author.username;
+        const totalDebt = DatabaseHelper.getValue("debt", username, message)
+        const userMoney = DatabaseHelper.getValue("dogeCoin", message.author.username, message);
+        const wantsToPayDownThisAmount = Number(args[0]);
+        if (!isNaN(wantsToPayDownThisAmount)) {
+            const newTotal = Number(totalDebt) - Number(args[0])
+            const userMasNumber = Number(userMoney);
+            if (userMasNumber < wantsToPayDownThisAmount) {
+                message.reply("du har ikke råd til dette.")
+                return;
+            } else {
+                DatabaseHelper.setValue("debt", username, newTotal.toFixed(2))
+                const newDogeCoinsCOunter = Number(userMoney) - wantsToPayDownThisAmount;
+                DatabaseHelper.setValue("dogeCoin", username, newDogeCoinsCOunter.toFixed(2))
+                MessageHelper.sendMessage(message, `Du har nå betalt ned ${wantsToPayDownThisAmount} av lånet ditt på ${totalDebt}. Lånet er nå på ${newTotal} og du har ${newDogeCoinsCOunter} coins igjen.`)
+            }
+        }
+        else {
+            message.reply("Du har ikke skrevet inn et tall");
+        }
     }
 
     static dealCoins(message: Message, value: string, peopleGettingCoins: string) {
@@ -162,6 +243,30 @@ export class GamblingCommands {
         isAdmin: true,
         command: (rawMessage: Message, messageContent: string, args: string[]) => {
             GamblingCommands.manageCoins(rawMessage, messageContent, args);
+        }
+    }
+    static readonly takeLoanCommand: ICommandElement = {
+        commandName: "lån",
+        description: "Lån 500 coins",
+
+        command: (rawMessage: Message, messageContent: string, args: string[]) => {
+            GamblingCommands.takeUpLoan(rawMessage, messageContent, args);
+        }
+    }
+    static readonly payDebtCommand: ICommandElement = {
+        commandName: "betal",
+        description: "Betal på lånet ditt. <number>",
+
+        command: (rawMessage: Message, messageContent: string, args: string[]) => {
+            GamblingCommands.payDownDebt(rawMessage, messageContent, args);
+        }
+    }
+    static readonly gambleCoins: ICommandElement = {
+        commandName: "gamble",
+        description: "Gambla coinså dine! Skriv inn mengde coins du vil gambla, så kan du vinna dobbelt hvis terningen trille 50 eller mer",
+
+        command: (rawMessage: Message, messageContent: string, args: string[]) => {
+            GamblingCommands.diceGamble(rawMessage, messageContent, args);
         }
     }
 
--- helpers/messageHelper.ts
+++ helpers/messageHelper.ts
@@ -1,111 +1,116 @@
-import { Channel, Client, DMChannel, Message, NewsChannel, TextChannel } from "discord.js";
+import { Channel, Client, DMChannel, Message, MessageEmbed, NewsChannel, TextChannel } from "discord.js";
 
 //import {client } from "./index"
 // import {action_log_channel} from "./index"
 // const Discord = require('discord.js');
 // const client = new Discord.Client();
 export type typeOfError = "unauthorized" | "error" | "warning";
+
 export class MessageHelper {
 
-	/**
-	 * Send message to the specified channel
-	 * @param rawMessage - A raw message type
-	 * @param message - The string to send
-	 * @param isError - (optional) if this is an error message, set to true
-	 * @param errorMsg - (optional) This message will be sent to the admin action log channel
-	 * @param typeOfError - (optional) The error message will depend on this type
-	 * return message - Returnerer message objectet som kan brukes (.edit(), .react() etc)
-	 */
-	static sendMessage(rawMessage: Message, message: string, isError?: boolean, errorMsg?: string, typeOfError?: typeOfError) {
-		const channel = rawMessage.channel as TextChannel;
-		// channel = channel as TextChannel;
+    /**
+     * Send message to the specified channel
+     * @param rawMessage - A raw message type
+     * @param message - The string to send
+     * @param isError - (optional) if this is an error message, set to true
+     * @param errorMsg - (optional) This message will be sent to the admin action log channel
+     * @param typeOfError - (optional) The error message will depend on this type
+     * return message - Returnerer message objectet som kan brukes (.edit(), .react() etc)
+     */
+    static sendMessage(rawMessage: Message, message: string, isError?: boolean, errorMsg?: string, typeOfError?: typeOfError) {
+        const channel = rawMessage.channel as TextChannel;
+        // channel = channel as TextChannel;
 
-		if (typeof message == "object") {
-			rawMessage.reply("Hmm .. her kom det en tom eller feilformattering melding. Hendelsen blir loggført så en nerd kan ta en skikk på hva som skjedde <" + message + ">");
-			MessageHelper.sendMessageToActionLog(channel, "En tom melding ble forsøkt sendt fra channel " + channel.name + ", forårsaket av en melding fra " + rawMessage.author.username + ". Meldingsinnhold: " + rawMessage.content)
-			return;
-		}
+        if (typeof message == "object") {
+            rawMessage.reply("Hmm .. her kom det en tom eller feilformattering melding. Hendelsen blir loggført så en nerd kan ta en skikk på hva som skjedde <" + message + ">");
+            MessageHelper.sendMessageToActionLog(channel, "En tom melding ble forsøkt sendt fra channel " + channel.name + ", forårsaket av en melding fra " + rawMessage.author.username + ". Meldingsinnhold: " + rawMessage.content)
+            return;
+        }
 
-		channel.type === "text"
-		if (!isError) {
-			try {
-				const msg = channel.send(message)
-				return msg;
-			} catch (error) {
-				return undefined
-			};
-		} else {
-			//First send message to channel it originated from
-			if (typeOfError == "unauthorized")
-				rawMessage.reply("Du er ikke autorisert til å utføre denne handlingen. Denne hendelsen blir loggført.")
-			else if (typeOfError == "error")
-				rawMessage.reply("Det har oppstått en feil. Feilkode er logget.")
+        channel.type === "text"
+        if (!isError) {
+            try {
+                const msg = channel.send(message)
+                return msg;
+            } catch (error) {
+                return undefined
+            };
+        } else {
+            //First send message to channel it originated from
+            if (typeOfError == "unauthorized")
+                rawMessage.reply("Du er ikke autorisert til å utføre denne handlingen. Denne hendelsen blir loggført.")
+            else if (typeOfError == "error")
+                rawMessage.reply("Det har oppstått en feil. Feilkode er logget.")
 
-			//Find action_log
-			const errorChannel = channel.client.channels.cache.get("810832760364859432") as TextChannel
-			//Send message to action_log
-			if (errorChannel) {
-				if (channel.type === "text") {
-					if (typeOfError == "warning")
-						errorChannel.send(channel.name + ": " + errorMsg)
-					else
-						errorChannel.send("En feil har oppstått i channelen " + channel.name + ". " + errorMsg)
-				}
-			}
-			return undefined
-		}
+            //Find action_log
+            const errorChannel = channel.client.channels.cache.get("810832760364859432") as TextChannel
+            //Send message to action_log
+            if (errorChannel) {
+                if (channel.type === "text") {
+                    if (typeOfError == "warning")
+                        errorChannel.send(channel.name + ": " + errorMsg)
+                    else
+                        errorChannel.send("En feil har oppstått i channelen " + channel.name + ". " + errorMsg)
+                }
+            }
+            return undefined
+        }
 
-	}
-	/** Reply til en gitt melding med gitt string. */
-	static replyToMessage(message: Message, errormessage: string) {
-		message.reply(errormessage)
-	}
-	/** Reply når feil formattering er brukt. Send inn hvilken formattering som skal brukes */
-	static replyFormattingError(message: Message, errormessage: string) {
-		message.reply("du har brukt feil formattering. Bruk: " + errormessage)
-	}
-	static async findMessageById(rawMessage: Message, id: string) {
-		const allChannels = rawMessage.client.channels.cache.array().filter(channel => channel instanceof TextChannel) as TextChannel[];
-		let messageToReturn;
+    }
+    /** Reply til en gitt melding med gitt string. */
+    static replyToMessage(message: Message, errormessage: string) {
+        message.reply(errormessage)
+    }
+    /** Reply når feil formattering er brukt. Send inn hvilken formattering som skal brukes */
+    static replyFormattingError(message: Message, errormessage: string) {
+        message.reply("du har brukt feil formattering. Bruk: " + errormessage)
+    }
+    static async findMessageById(rawMessage: Message, id: string) {
+        const allChannels = rawMessage.client.channels.cache.array().filter(channel => channel instanceof TextChannel) as TextChannel[];
+        let messageToReturn;
 
-		for (const channel of allChannels) {
-			if (channel) {
-				await channel.messages.fetch(id).then(message => {
-					if (message.guild) {
-						messageToReturn = message;
-					}
-				}).catch((error) => {
-				})
-			}
-		}
-		return messageToReturn
-	}
+        for (const channel of allChannels) {
+            if (channel) {
+                await channel.messages.fetch(id).then(message => {
+                    if (message.guild) {
+                        messageToReturn = message;
+                    }
+                }).catch((error) => {
+                })
+            }
+        }
+        return messageToReturn
+    }
 
-	static sendMessageToActionLog(channel: TextChannel, msg: string) {
-		const errorChannel = channel.client.channels.cache.get("810832760364859432") as TextChannel
-		errorChannel.send(msg);
-	}
-	static sendMessageToBotUtvikling(channel: TextChannel) {
-		const errorChannel = channel.client.channels.cache.get("802716150484041751") as TextChannel
-		errorChannel.send("Logget på");
-	}
-	static sendErrorMessage(text: string, channel?: TextChannel) {
-		if (channel) {
-			channel.send(text)
-		}
-		// const errorChannel = mazariniClient.channels.cache.get("810832760364859432");
-	}
+    static async sendFormattedMessage(message: Message, newMessage: MessageEmbed) {
+        message.channel.send(newMessage)
+    }
 
-	//Oldchannel er brukt for å ha en referanse til client
-	static sendMessageToSpecificChannel(channelId: string, text: string, oldChannel: TextChannel) {
+    static sendMessageToActionLog(channel: TextChannel, msg: string) {
+        const errorChannel = channel.client.channels.cache.get("810832760364859432") as TextChannel
+        errorChannel.send(msg);
+    }
+    static sendMessageToBotUtvikling(channel: TextChannel) {
+        const errorChannel = channel.client.channels.cache.get("802716150484041751") as TextChannel
+        errorChannel.send("Logget på");
+    }
+    static sendErrorMessage(text: string, channel?: TextChannel) {
+        if (channel) {
+            channel.send(text)
+        }
+        // const errorChannel = mazariniClient.channels.cache.get("810832760364859432");
+    }
 
-		const msgChannel = oldChannel.client.channels.cache.get(channelId.trim()) as TextChannel
-		if (msgChannel) {
-			msgChannel.send(text.trim())
-		}
-		else {
-			oldChannel.send("Ingen text channel ble funnet på oppgitt id <" + channelId + ">.")
-		}
-	}
+    //Oldchannel er brukt for å ha en referanse til client
+    static sendMessageToSpecificChannel(channelId: string, text: string, oldChannel: TextChannel) {
+
+        const msgChannel = oldChannel.client.channels.cache.get(channelId.trim()) as TextChannel
+        if (msgChannel) {
+            msgChannel.send(text.trim())
+        }
+        else {
+            oldChannel.send("Ingen text channel ble funnet på oppgitt id <" + channelId + ">.")
+        }
+    }
 
 }
