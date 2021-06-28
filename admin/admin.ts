d41c40144c9d9eb72a035218d7f4ce8328489358
Arne Kvaleberg <arne@papirfly.no>
2021-06-28 19:38:07 +0200

    replacing

--- admin/admin.ts
+++ admin/admin.ts
@@ -150,7 +150,7 @@ export class Admin {
         const user = DatabaseHelper.findUserByUsername(username, message);// message.client.users.cache.find(user => user.username == username);
         // const id = c[0].trim();
 
-        const replyString = messageContent.substr(messageContent.indexOf(username) + username.length + 3);
+        const replyString = messageContent.replace(username, "")//substr(messageContent.indexOf(username) + username.length + 1);
         if (user) {
             if (user.username == message.author.username) {
                 message.reply("Du kan kje warna deg sjøl, bro")
