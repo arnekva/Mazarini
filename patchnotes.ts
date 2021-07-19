6033e75be94aa60489264d702083bd740a25eb08
Arne Kvaleberg <arnekva@gmail.com>
2021-07-19 11:24:46 +0200

    patch

--- patchnotes.ts
+++ patchnotes.ts
@@ -9,7 +9,7 @@ export class PatchNotes {
      * Increment y for mindre oppdateringer (enkle funksjoner osv)
      * Increment z for bugfixes, mindre tekstendringer, sannsynlighetsendringer etc
      */
-    public static readonly currentVersion = "2.9.1";
+    public static readonly currentVersion = "2.9.2";
 
     /** Private, brukes kun av getCurrentPatchNotes */
     private static readonly header = "Patch notes for versjon " + PatchNotes.currentVersion;
@@ -27,6 +27,7 @@ export class PatchNotes {
         + "\n * Du kan ikke lenger ha linebreaks i statusene dine (blir erstattet med mellomrom)"
         + "\n * Ukens spinner-vinner får nå +1000 chips. Ukens taper mister 500. "
         + "\n * Fikset en liten feil i messagehelper som kunne føre til kræsj."
+        + "\n * Jærsk-ifyer lagt til '!mz jærsk <text|id>."
 
         ;
 
