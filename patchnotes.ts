8a3a17f8c72cb9020544021ad58d5eb2965f46b0
arnekva <arnekva@gmail.com>
2021-12-06 21:19:50 +0100

    Including patch notes

--- patchnotes.ts
+++ patchnotes.ts
@@ -2,7 +2,7 @@ import { Channel, Client, DMChannel, NewsChannel, TextChannel, Message, MessageF
 import { ICommandElement } from './commands/commands'
 import { MessageHelper } from './helpers/messageHelper'
 export class PatchNotes {
-    public static readonly currentVersion = '3.8.4'
+    public static readonly currentVersion = '3.8.5'
 
     private static readonly header = 'Patch notes for versjon ' + PatchNotes.currentVersion
 
