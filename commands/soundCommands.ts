875071004136c4aaa5c1ee22c3aac9a0dc6b8447
Arne Kvaleberg <arnekva@gmail.com>
2022-02-19 14:05:08 +0100

    Fix typing

--- commands/soundCommands.ts
+++ commands/soundCommands.ts
@@ -61,7 +61,7 @@ export class SoundCommands extends AbstractCommands {
                 }
             })
         } catch (error) {
-            message.reply(error)
+            message.reply('Error: ' + error)
         }
     }
 
