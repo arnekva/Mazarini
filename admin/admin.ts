7e4efc731fb2ed1f7bd3856754371c5450685a3b
Arne Kvaleberg <arnekva@gmail.com>
2022-03-24 19:17:41 +0100

    Add crash event for testing

--- admin/admin.ts
+++ admin/admin.ts
@@ -252,11 +252,12 @@ export class Admin extends AbstractCommands {
                 description: 'For testing. Resultat vil variere. ',
                 hideFromListing: true,
                 command: async (rawMessage: Message, messageContent: string) => {
+                    rawMessage.reply('')
                     // DailyJobs.validateAndResetDailyClaims()
-                    rawMessage.reply('Slettet alle coins og chips for bruker <' + rawMessage.author.username + '>.')
-                    setTimeout(() => {
-                        rawMessage.reply('Bare kødda, ingenting har skjedd.')
-                    }, 7000)
+                    // rawMessage.reply('Slettet alle coins og chips for bruker <' + rawMessage.author.username + '>.')
+                    // setTimeout(() => {
+                    //     rawMessage.reply('Bare kødda, ingenting har skjedd.')
+                    // }, 7000)
                 },
                 category: 'admin',
             },
