3c9a77bbe164fc78dc6ddc921668ea546c3c389a
Arne Kvaleberg <arnekva@gmail.com>
2022-03-31 22:45:38 +0200

    Patching

--- patchnotes.ts
+++ patchnotes.ts
@@ -3,7 +3,7 @@ import { AbstractCommands } from './Abstracts/AbstractCommand'
 import { ICommandElement } from './General/commands'
 import { MessageHelper } from './helpers/messageHelper'
 export class PatchNotes extends AbstractCommands {
-    public static readonly currentVersion = '6.5.0'
+    public static readonly currentVersion = '6.5.1'
 
     private static readonly header = 'Patch notes for versjon ' + PatchNotes.currentVersion
 
