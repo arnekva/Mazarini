f483a884da9ef85291bcad2a042feccef51c7132
Arne Kvaleberg <arnekva@gmail.com>
2022-05-13 21:03:38 +0200

    Random helg message

--- Jobs/dayJobs.ts
+++ Jobs/dayJobs.ts
@@ -1,4 +1,5 @@
 import { MessageHelper } from '../helpers/messageHelper'
+import { ArrayUtils } from '../utils/arrayUtils'
 import { MessageUtils } from '../utils/messageUtils'
 
 type dayJobs = 'friday' | 'none'
@@ -18,11 +19,23 @@ export class DayJob {
 
     private itsWeekend() {
         const todaysTime = new Date().toLocaleTimeString()
-        this.messageHelper.sendMessage(MessageUtils.CHANNEL_IDs.GENERAL, `Det e helg!`)
+        this.messageHelper.sendMessage(MessageUtils.CHANNEL_IDs.GENERAL, `${ArrayUtils.randomChoiceFromArray(this.getHelgMessage())}`)
         console.log(`Sendte melding til General om helg. ${todaysTime}`)
     }
     private logEvent() {
         const todaysTime = new Date().toLocaleTimeString()
         this.messageHelper.sendMessage('810832760364859432', `Dag-job for ${this.day} kjørte ${todaysTime} `)
+    }
+
+    private getHelgMessage() {
+        return [
+            'Aellen, det e helg gutta',
+            'Det e mådderfåkkings heeeeelg',
+            'HEEEEEEEEEEEEEEEEEEEEEEEEELG',
+            'Komma dokk hjem folkens, det e helg',
+            'omw dirr hjem for å krækka åpen a cold one, det e helg boys',
+            'ud å fyra elle? Ejo helg',
+            'Helg folkens. Skru av maskinane og komma seg hjem asap',
+        ]
     }
 }
