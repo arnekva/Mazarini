2da35a0b6a9845aaaceb8916ac2766a0bc9eae13
Sivert Lunde <sivert.lunde@knowit.no>
2023-03-06 17:27:43 +0100

    Sjekk patchnotes

---
+++ commands/testCommands.ts
@@ -0,0 +1,113 @@
+import { ActionRowBuilder, ButtonBuilder, ButtonInteraction, ButtonStyle, CacheType, ChatInputCommandInteraction, Client, EmbedBuilder, Message } from 'discord.js'
+import { AbstractCommands } from '../Abstracts/AbstractCommand'
+import { ICommandElement, IInteractionElement } from '../general/commands'
+import { ButtonHandler } from '../handlers/buttonHandler'
+import { MessageHelper } from '../helpers/messageHelper'
+
+const defaultButtonRow = new ActionRowBuilder<ButtonBuilder>()
+defaultButtonRow.addComponents(
+    new ButtonBuilder({
+        custom_id: `${ButtonHandler.TEST}`,
+        style: ButtonStyle.Primary,
+        label: `Test`,
+        disabled: false,
+        type: 2,
+    })
+)
+
+//NB: IKKE PUSH ENDRINGER I DENNE KLASSEN MED MINDRE DET ER GENERISKE HJELPEMETODER
+
+//Skall-klasse for testing av alt mulig random shit. 
+export class TestCommands extends AbstractCommands {
+    private embedMessage: Message
+    private buttonsMessage: Message
+    private embed: EmbedBuilder
+    private currentButtons: ActionRowBuilder<ButtonBuilder>
+
+    constructor(client: Client, messageHelper: MessageHelper) {
+        super(client, messageHelper)
+        this.embedMessage = undefined
+        this.buttonsMessage = undefined
+        this.embed = undefined
+        this.currentButtons = defaultButtonRow
+    }
+
+    public async test(interaction: ChatInputCommandInteraction<CacheType> | ButtonInteraction<CacheType>) {
+        this.messageHelper.replyToInteraction(interaction, "Disse kommandoene brukes bare for lokal testing og er ikke implementerte ellers.", false)
+    }
+
+    //Redigerer eksisterende embed hvis det er en knapp interaction, sender ny embed hvis ikke
+    private async replyToInteraction(interaction: ChatInputCommandInteraction<CacheType> | ButtonInteraction<CacheType>) {
+        if (interaction.isButton()) {
+            this.embedMessage.edit({embeds: [this.embed]})
+            interaction.deferUpdate()
+        } else {
+            this.messageHelper.replyToInteraction(interaction, "Test")
+            this.embedMessage = await this.messageHelper.sendFormattedMessage(interaction.channelId, this.embed)
+            this.buttonsMessage = await this.messageHelper.sendMessageWithComponents(interaction.channelId, [this.currentButtons])
+        }
+    }
+
+    //Flytt embed ned til bunnen
+    private async resendMessages(interaction: ButtonInteraction<CacheType>) {
+        this.deleteMessages()
+        this.embedMessage = await this.messageHelper.sendFormattedMessage(interaction.channelId, this.embed)
+        this.buttonsMessage = await this.messageHelper.sendMessageWithComponents(interaction.channelId, [this.currentButtons])
+    }
+
+    //Slett meldingene
+    private deleteMessages() {
+        this.embedMessage.delete()
+        this.buttonsMessage.delete()
+        this.embedMessage = undefined
+        this.buttonsMessage = undefined
+    }
+
+    private testSwitch(interaction: ChatInputCommandInteraction<CacheType>) {
+        const action = interaction.options.getSubcommand()        
+        if (action) {
+            switch (action.toLowerCase()) {
+                case '-1-': {
+                    this.test(interaction)
+                    break
+                }
+                case '-2-': {
+                    this.test(interaction)
+                    break
+                }
+                case '-3-': {
+                    this.test(interaction)
+                    break
+                }
+                case '-4-': {
+                    this.test(interaction)
+                    break
+                }
+                default: {
+                    this.messageHelper.replyToInteraction(interaction, "Default test sub-command")
+                }
+            }
+        } else {
+            this.messageHelper.replyToInteraction(
+                interaction,
+                "Ingen test sub-command angitt"
+            )
+        }
+    }
+
+    public getAllCommands(): ICommandElement[] {
+        return []
+    }
+
+    getAllInteractions(): IInteractionElement[] {
+        return [
+            {
+                commandName: 'test',
+                command: (rawInteraction: ChatInputCommandInteraction<CacheType>) => {
+                    this.testSwitch(rawInteraction)
+                },
+                category: 'annet',
+            },
+        ]
+    }
+}
