9fd142432a863c84a9e27b35c58b78ba306861e5
Arne Kvaleberg <arne@papirfly.no>
2023-03-14 15:09:27 +0100

    Fix case sensitive search

--- commands/textCommands.ts
+++ commands/textCommands.ts
@@ -11,22 +11,39 @@ export class TextCommands extends AbstractCommands {
     }
 
     private async searchWiki(interaction: ChatInputCommandInteraction<CacheType>) {
-        await interaction.deferReply()
+        // await interaction.deferReply()
         const search = interaction.options.get('search')?.value as string
 
-        try {
-            const result = await wiki({ apiUrl: 'https://no.wikipedia.org/w/api.php' })?.page(search)
-            if (result) {
-                const summary = (await result.summary()).slice(0, 500) + '..'
-                const image = await result.mainImage()
+        const capitalizeFirstLetter = (text: string) => {
+            if (text.charAt(0).toUpperCase() === text.charAt(0)) return text.charAt(0).toLowerCase() + text.slice(1)
+            return text.charAt(0).toUpperCase() + text.slice(1)
+        }
 
-                const embed = EmbedUtils.createSimpleEmbed(`${search}`, summary)
-                if (image) embed.setThumbnail(image)
-                embed.setURL(result.url())
-                this.messageHelper.replyToInteraction(interaction, embed, false, true)
-            }
+        let result
+        try {
+            result = await wiki({ apiUrl: 'https://no.wikipedia.org/w/api.php' })?.page(search)
         } catch (error) {
-            this.messageHelper.replyToInteraction(interaction, `Fant ingen sider på *${search}*`, false, true)
+            const splitSearch = search.split(' ').map((word) => capitalizeFirstLetter(word))
+            const search2 = splitSearch.join(' ')
+            try {
+                result = await wiki({ apiUrl: 'https://no.wikipedia.org/w/api.php' })?.page(search2)
+            } catch (error) {
+                this.messageHelper.replyToInteraction(
+                    interaction,
+                    `Fant ingen sider på *${search}* eller *${search2}*. Husk at søket er case sensitive`,
+                    false,
+                    true
+                )
+            }
+        }
+        if (result) {
+            const summary = (await result.summary()).slice(0, 450) + '...'
+            const image = await result.mainImage()
+            const embed = EmbedUtils.createSimpleEmbed(`${search}`, summary)
+           
+            if (image) embed.setThumbnail(image)
+            embed.setURL(result.url())
+            this.messageHelper.replyToInteraction(interaction, embed, false, true)
         }
     }
 
--- helpers/messageHelper.ts
+++ helpers/messageHelper.ts
@@ -63,7 +63,7 @@ export class MessageHelper {
     ): Promise<boolean> {
         const handleError = async (e: any) => {
             let msg: Message<boolean> | undefined
-            if (content instanceof EmbedBuilder) msg = await this.sendFormattedMessage(interaction.channelId, content)
+            if (typeof content === 'object') msg = await this.sendFormattedMessage(interaction.channelId, content)
             else msg = await this.sendMessage(interaction.channelId, `${MentionUtils.mentionUser(interaction.user.id)} ${content}`)
 
             let msgInfo = msg ? `Sendte en separat melding i stedet for interaksjonssvar.` : `Klarte heller ikke sende separat melding som svar`
