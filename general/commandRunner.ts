d5e56f1ff97cf825d9601c89cf2768300c3d68f2
Sivert Lunde <sivert.lunde@knowit.no>
2023-07-25 19:03:22 +0200

    whops

--- general/commandRunner.ts
+++ general/commandRunner.ts
@@ -301,7 +301,7 @@ export class CommandRunner {
         return (
             (environment === 'dev' &&
                 (interaction?.channel.id === MentionUtils.CHANNEL_IDs.LOKAL_BOT_SPAM ||
-                    interaction?.channel.id === MentionUtils.CHANNEL_IDs.DEV_LOKAL_BOT ||
+                    interaction?.channel.id === MentionUtils.CHANNEL_IDs.LOKAL_BOT_SPAM_DEV ||
                     interaction?.channel.id === MentionUtils.CHANNEL_IDs.STATS_SPAM ||
                     interaction?.channel.id === MentionUtils.CHANNEL_IDs.GODMODE)) ||
             (environment === 'prod' &&
