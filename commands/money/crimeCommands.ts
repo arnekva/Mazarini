aa35e5e2dc8712349a99275336d4ff94ee436041
Arne Kvaleberg <arne@papirfly.no>
2023-11-03 08:32:24 +0100

    fix float in bribe

--- commands/money/crimeCommands.ts
+++ commands/money/crimeCommands.ts
@@ -355,7 +355,7 @@ export class CrimeCommands extends AbstractCommands {
         } else {
             if (isBribe) {
                 const userChips = prisoner.chips
-                const bribePrice = Math.max(userChips * 0.2, 10000)
+                const bribePrice = Math.floor(Math.max(userChips * 0.2, 10000))
                 const canBribe = jailState !== 'max'
                 if (userChips < bribePrice || !canBribe) {
                     return this.messageHelper.replyToInteraction(
