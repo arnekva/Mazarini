f0c357ffd8d563d23ad88a28179569250d9e2119
Arne Kvaleberg <arne@papirfly.no>
2023-11-22 10:06:42 +0100

    notes

--- commands/patchnotes/patchnotes.ts
+++ commands/patchnotes/patchnotes.ts
@@ -7,8 +7,8 @@ import { DatabaseHelper } from '../../helpers/databaseHelper'
 import { MessageHelper } from '../../helpers/messageHelper'
 import { MentionUtils } from '../../utils/mentionUtils'
 export class PatchNotes extends AbstractCommands {
-    public static readonly currentVersion = '14.1.1'
-    public static readonly currentPatchNotes = `\n* Økt utbetalinger i /roll og /spin`
+    public static readonly currentVersion = '14.1.2'
+    public static readonly currentPatchNotes = `\n* Mindre endringer`
 
     private static readonly header = 'Patch notes for versjon ' + PatchNotes.currentVersion
     public static readonly trelloBoardUrl = `https://trello.com/b/g4KkZwaX/bot-h%C3%B8ie`
