e47be5753fc7966bdd225be378fbcc1ca2e64e9f
Arne Kvaleberg <arne@papirfly.no>
2023-11-27 17:56:55 +0100

    Update wrong import

--- utils/textUtils.ts
+++ utils/textUtils.ts
@@ -1,13 +1,6 @@
-import * as cleanTextUtils from 'clean-text-utils'
 import { Message } from 'discord.js'
 
 export namespace TextUtils {
-    export function escapeString(text: string) {
-        let cleanKey = cleanTextUtils.strip.nonASCII(text)
-        // cleanKey = cleanTextUtils.replace.exoticChars(text);
-        return cleanKey
-    }
-
     export function isUpperCase(str: string) {
         return str === str.toUpperCase()
     }
