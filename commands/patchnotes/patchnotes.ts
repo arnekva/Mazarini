9d27391de69e688c2043e3007a81d4b8ad7c849a
Arne Kvaleberg <arne@papirfly.no>
2023-12-29 22:00:18 +0100

    Add patchnotes

--- commands/patchnotes/patchnotes.ts
+++ commands/patchnotes/patchnotes.ts
@@ -7,9 +7,8 @@ import { DatabaseHelper } from '../../helpers/databaseHelper'
 import { MessageHelper } from '../../helpers/messageHelper'
 import { ChannelIds } from '../../utils/mentionUtils'
 export class PatchNotes extends AbstractCommands {
-    public static readonly currentVersion = '15.2.1'
-    public static readonly currentPatchNotes =
-        `\n* /vivino viser nå enda mer data` + `\n* Fikset en feil som gjorde at bare siste commit-melding ble logget, i stedet for alle nye`
+    public static readonly currentVersion = '15.2.2'
+    public static readonly currentPatchNotes = `\n* Fikset en feil som gjorde at /helg ikke ga beskjed om langhelg hvis helligdagen lå i et annet år enn helgen`
 
     private static readonly header = 'Patch notes for versjon ' + PatchNotes.currentVersion
     public static readonly trelloBoardUrl = `https://trello.com/b/g4KkZwaX/bot-h%C3%B8ie`
