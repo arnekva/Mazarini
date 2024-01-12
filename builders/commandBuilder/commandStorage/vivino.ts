5de5c7741cc4a9c964e25fccf831e78b51710c34
Arne Kvaleberg <arnekva@gmail.com>
2024-01-12 16:53:23 +0100

    Can now choose specific year for vivino

--- builders/commandBuilder/commandStorage/vivino.ts
+++ builders/commandBuilder/commandStorage/vivino.ts
@@ -1,7 +1,29 @@
+import { ApplicationCommandOptionType } from 'discord.js'
 import { ISlashCommandItem } from '../commandBuilder'
 
 /** Saved version of the Vivino command */
 export const vivinoCommand: ISlashCommandItem = {
     commandName: 'vivino',
     commandDescription: 'se vin-året ditt',
+    options: [
+        {
+            name: 'year',
+            description: 'årstall du vil sjekke for',
+            type: ApplicationCommandOptionType.Number,
+            required: false,
+            choices: [
+                {
+                    name: '2024',
+                    value: 2024,
+                },
+                {
+                    name: '2023',
+                    value: 2023,
+                },
+                {
+                    name: '2022',
+                    value: 2022,
+                },
+            ]
+        },]
 }
