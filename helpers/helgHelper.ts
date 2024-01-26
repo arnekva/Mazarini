2ccec37793a1440bc27dcc6e4ef9930880b221bd
Sivert Lunde <sivert.lunde@knowit.no>
2024-01-26 14:25:42 +0100

    adjust multiplier in helg feeling calculation

--- helpers/helgHelper.ts
+++ helpers/helgHelper.ts
@@ -119,8 +119,8 @@ export namespace HelgHelper {
         const hours = date.hour()
         const minutes = date.minute()
         const seconds = date.second()
-        const currentSecond = ((day * 24 + hours) * 60 + minutes) * 60 + seconds
-        const input = currentSecond * 0.000246
+        const currentSecond = ((day * 24 + hours) * 60 + minutes) * 60 + seconds        
+        const input = currentSecond * 0.00024645365
         let percentage = +(((input - 20) ** 3 / 10000) * 2).toFixed(2)
         if (percentage < 90) percentage = Math.floor(percentage)
         return Math.max(Math.min(percentage, 100), 0)
