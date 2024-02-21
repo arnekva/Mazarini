0eb92ebb46164ee2e3e3856b96b1e5f19dcdf186
Arne Kvaleberg <arnekva@gmail.com>
2024-02-21 11:04:34 +0100

    Label fix

--- commands/money/moneyCommands.ts
+++ commands/money/moneyCommands.ts
@@ -149,7 +149,7 @@ export class MoneyCommands extends AbstractCommands {
 
     private findPrestigeMultiplier(p: number | undefined) {
         if (p && !isNaN(p) && p > 0) {
-            return 1 + 0.305 * p
+            return 1 + 0.3275 * p
         }
         return 1
     }
