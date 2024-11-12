c2a7fc76a58315027bdbe16d658753076495391a
Arne Kvaleberg <arne@papirfly.no>
2024-11-12 15:01:15 +0100

    Fix emoji regex

--- general/mazariniTracker.ts
+++ general/mazariniTracker.ts
@@ -4,7 +4,7 @@ import { ServerIds } from '../utils/mentionUtils'
 
 export class MazariniTracker {
     private client: MazariniClient
-    emojiRegex = new RegExp(/<(a)?:(\S+):(\d+)>/gi)
+    emojiRegex = new RegExp(/<(a)?:([a-zA-Z0-9_]+):(\d+)>/gi)
 
     constructor(client: MazariniClient) {
         this.client = client
@@ -17,7 +17,7 @@ export class MazariniTracker {
             const emojiNames: string[] = []
             while ((match = this.emojiRegex.exec(message.content))) {
                 if (match && message.guild.emojis.cache.get(match[3])) emojiNames.push(match[2])
-            }            
+            }
             if (emojiNames) this.client.database.updateEmojiMessageCounters(emojiNames)
         }
     }
