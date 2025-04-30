02b47b13dbd5b13a47eb1a1c9895946caefe1bae
Arne Kvaleberg <arne@papirfly.no>
2025-04-30 10:24:33 +0200

    Patchnotes

--- patchnotes.ts
+++ patchnotes.ts
@@ -8,12 +8,8 @@ import { IInteractionElement } from './interfaces/interactionInterface'
 import { ChannelIds } from './utils/mentionUtils'
 
 export class PatchNotes extends AbstractCommands {
-    public static readonly currentVersion = '25.0.0'
-    public static readonly currentPatchNotes =
-        '* Startet overgang til ny, finere komponent.' +
-        '\n  * Først ut er loot reveal' +
-        '\n      * Denne er også vesentlig raskere nå' +
-        '\n* Lagt til "Flytt ned" knapp på blackjack'
+    public static readonly currentVersion = '25.1.0'
+    public static readonly currentPatchNotes = '* Du kan nå sjekke Deal or no Deal stats med "/stats bruker"' + '\n  * Du vil kun se stats f.o.m 29.04'
 
     private static readonly header = 'Patch notes for versjon ' + PatchNotes.currentVersion
     public static readonly trelloBoardUrl = `https://trello.com/b/g4KkZwaX/bot-h%C3%B8ie`
