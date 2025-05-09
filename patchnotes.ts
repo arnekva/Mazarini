ff9fe842791537c536a239e520b9888fc5cbfb8f
Arne Kvaleberg <arne@papirfly.no>
2025-05-09 07:49:07 +0200

    no longer hide best attempt

--- patchnotes.ts
+++ patchnotes.ts
@@ -8,8 +8,8 @@ import { IInteractionElement } from './interfaces/interactionInterface'
 import { ChannelIds } from './utils/mentionUtils'
 
 export class PatchNotes extends AbstractCommands {
-    public static readonly currentVersion = '25.2.2'
-    public static readonly currentPatchNotes = '* Høie vil nå ignorere nye games som ikke starter på 10k+'
+    public static readonly currentVersion = '25.2.3'
+    public static readonly currentPatchNotes = '* Beste forsøk vil ikke lenger være skjult i MoL'
 
     private static readonly header = 'Patch notes for versjon ' + PatchNotes.currentVersion
     public static readonly trelloBoardUrl = `https://trello.com/b/g4KkZwaX/bot-h%C3%B8ie`
