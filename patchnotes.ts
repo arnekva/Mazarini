f3f7739618275afcadca00db726279263e0da7f6
Arne Kvaleberg <arne@papirfly.no>
2025-06-11 10:54:55 +0200

    formatting

--- patchnotes.ts
+++ patchnotes.ts
@@ -8,14 +8,15 @@ import { IInteractionElement } from './interfaces/interactionInterface'
 import { ChannelIds } from './utils/mentionUtils'
 
 export class PatchNotes extends AbstractCommands {
-    public static readonly currentVersion = '26.2.0'
+    public static readonly currentVersion = '26.2.1'
     public static readonly currentPatchNotes =
         `* More or Less:` +
-        `   * Hvert nivå gir nå 100 mindre chips (eks vil 1-10 nå gi 400 heller enn 500)` +
+        `  * Hvert nivå gir nå 100 mindre chips (eks vil 1-10 nå gi 400 heller enn 500)` +
         `* Små multiplier nerfs til terning:` +
-        `   * Streak er nå x1000 i stedet for x1100 (eks. streak på 5 er nå 1000 i stedet for 1100)` +
-        `   * ATH Streak er nå x1500 i stedet for x2500` +
-        `   * ATH smell på tall er nå x35 i stedet for x50`
+        `  * Streak er nå x1000 i stedet for x1100 (eks. streak på 5 er nå 1000 i stedet for 1100)` +
+        `  * ATH Streak er nå x1500 i stedet for x2500` +
+        `  * ATH smell på tall er nå x35 i stedet for x50` +
+        `* Fikset formattering på patch notes`
 
     private static readonly header = 'Patch notes for versjon ' + PatchNotes.currentVersion
     public static readonly trelloBoardUrl = `https://trello.com/b/g4KkZwaX/bot-h%C3%B8ie`
