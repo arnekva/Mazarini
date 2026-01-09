d7ee4f3afbe73c8b30bfdb48c38036a3491f9d3e
Arne Kvaleberg <arnekva@gmail.com>
2026-01-09 20:16:59 +0100

    version dump

--- patchnotes.ts
+++ patchnotes.ts
@@ -16,7 +16,7 @@ export class PatchNotes extends AbstractCommands {
         super(client)
     }
 
-    public static readonly currentVersion = '29.3.2'
+    public static readonly currentVersion = '29.3.3'
 
     static getCurrentPatchNotes() {
         const container = new SimpleContainer()
