be17a987e10f5c13ac0d7bade873af4ce48ea10e
Arne Kvaleberg <arnekva@gmail.com>
2026-09-23 10:47:07 +0200

    Increase effect item chance in Deal Or No Deal

--- general/values.ts
+++ general/values.ts
@@ -398,7 +398,7 @@ export const GameValues: GameValuesType = {
 
     // Deal Or No Deal
     dealOrNoDeal: {
-        effectItemChance: 35, // percent chance to get an effect item
+        effectItemChance: 50, // percent chance to get an effect item
         offerBase: 0.5, // base offer percentage
         offerPerRound: 0.05, // offer percentage increase per round
     },
